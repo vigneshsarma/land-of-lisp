229cdf48381f82db44004b977d3f3d6bc509194e
Vignesh Sarma K <vignesh.sarma@gmail.com>
2013-01-13 12:16:18 +0530

    Basic test game engine completed.

--- text_game_engine.lisp
+++ text_game_engine.lisp
@@ -42,4 +42,28 @@
                          `(you see a ,obj on the floor.)))
     (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
 
-(describe-objects 'living-room *objects* *object-locations*)
+(defun look ()
+  (append (describe-location *location* *nodes*)
+          (describe-paths *location* *edges*)
+          (describe-objects *location* *objects* *object-locations*)))
+(look)
+
+(defun walk (direction)
+  (let ((next (find direction
+                    (cdr (assoc *location* *edges*))
+                    :key #'cadr)))
+    (if next
+        (progn (setf *location* (car next))
+               (look))
+      '(you cannot go that way.))))
+
+(defun pickup (object)
+  (cond ((member object
+                 (objects-at *location* *objects* *object-locations*))
+         (push (list object 'body) *object-locations*)
+         `(you are now carrying the ,object))
+        (t '(you cannot get that.))))
+
+(defun inventory ()
+  (cons 'items- (objects-at 'body *objects* *object-locations*)))
+;; (describe-objects 'living-room *objects* *object-locations*)
